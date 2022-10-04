86979aaf90da3b70dadba2ff2da25aeae69c59b1
dbajenovcapitolcanary <dbajenov@capitolcanary.com>
2022-10-04 14:29:37 +0300

    Adaptive

--- js/swiper.js
+++ js/swiper.js
@@ -11,6 +11,28 @@ const swiper = new Swiper('.swiper', {
    pagination: {
      el: '.swiper-pagination',
    },
+
+   breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50
+    }
+  },
  
    // Navigation arrows
    navigation: {
