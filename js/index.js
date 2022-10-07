43a6f1618d778362f3f85712b1b6255fdcedfb55
dbajenovcapitolcanary <dbajenov@capitolcanary.com>
2022-10-07 17:55:09 +0300

    UR Add lng features

--- js/index.js
+++ js/index.js
@@ -8,11 +8,17 @@ $(document).ready(function(){
    $("#mdTranslator").click(function(){
      translate('md', 'lng-tag');
    });
+   $("#ruTranslator").click(function(){
+    translate('ru', 'lng-tag');
+  });
    $("#enTranslatorMobile").click(function(){
       translate('en', 'lng-tag');
     });
     $("#mdTranslatorMobile").click(function(){
       translate('md', 'lng-tag');
+    });
+    $("#ruTranslatorMobile").click(function(){
+      translate('ru', 'lng-tag');
     });
 });
 
@@ -23,17 +29,31 @@ function translate(lng, tagAttr){
    if(lng == 'en'){
      $("#enTranslator").css('color', '#FFFFFF');
      $("#mdTranslator").css('color', '#424242');
+     $("#ruTranslator").css('color', '#424242');
    } 
    if(lng == 'md'){
      $("#mdTranslator").css('color', '#FFFFFF');
      $("#enTranslator").css('color', '#424242');
+     $("#ruTranslator").css('color', '#424242');
    }
+   if(lng == 'ru'){
+    $("#ruTranslator").css('color', '#FFFFFF');
+    $("#enTranslator").css('color', '#424242');
+    $("#mdTranslator").css('color', '#424242');
+  }
    if(lng == 'en'){
       $("#enTranslatorMobile").css('color', '#FFFFFF');
       $("#mdTranslatorMobile").css('color', '#424242');
+      $("#ruTranslatorMobile").css('color', '#424242');
     } 
     if(lng == 'md'){
       $("#mdTranslatorMobile").css('color', '#FFFFFF');
+      $("#enTranslatorMobile").css('color', '#424242');
+      $("#ruTranslatorMobile").css('color', '#424242');
+    }
+    if(lng == 'ru'){
+      $("#ruTranslatorMobile").css('color', '#FFFFFF');
+      $("#mdTranslatorMobile").css('color', '#424242');
       $("#enTranslatorMobile").css('color', '#424242');
     }
 }
